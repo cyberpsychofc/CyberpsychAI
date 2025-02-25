ef6c81061a9a400947be394807d178b3d657b95b
cyberpsychofc <aryan2003k@gmail.com>
2025-02-25 09:42:41 +0530

    flow change prod test 2

--- CyberpsychBot/server.py
+++ CyberpsychBot/server.py
@@ -7,8 +7,8 @@ from tweet import tweet, reply
 bot = Flask(__name__)
 
 # Free-tier sends atmost of 17 requests a day, so plan
-post_times = ["09:39"]
-roast_times = ["09:42"]
+post_times = ["09:47"]
+roast_times = ["09:49"]
 
 @bot.route("/")
 def home():
@@ -23,13 +23,10 @@ def tweet_job():
     for post in post_times:
         schedule.every().day.at(post).do(tweet)
     for roast in roast_times:
-        schedule.every().day.at(roast).do(reply)
+        schedule.every().day.at(roast).do(reply)   
 
-@bot.before_first_request
-def start_scheduler():
+if __name__ == "__main__":
     tweet_job()
     task = threading.Thread(target=run_scheduler, daemon=True)
     task.start()
-
-if __name__ == "__main__":
     bot.run(host="0.0.0.0", port=8000)
