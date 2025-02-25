8e80d4a34c5f917096720103c41ee966955c237a
cyberpsychofc <aryan2003k@gmail.com>
2025-02-25 09:35:40 +0530

    changed flow test

--- CyberpsychBot/server.py
+++ CyberpsychBot/server.py
@@ -5,22 +5,31 @@ from flask import Flask
 from tweet import tweet, reply
 
 bot = Flask(__name__)
-flag = True
+
+# Free-tier sends atmost of 17 requests a day, so plan
+post_times = ["09:39"]
+roast_times = ["09:42"]
+
 @bot.route("/")
 def home():
     return "CyberpsychAI is running!"
 
-def tweet_job():
-    global flag
-    if flag:
-        schedule.every(2).minutes.do(tweet)
-        flag = False
-    schedule.every(480).minutes.do(reply)
+def run_scheduler():
     while True:
-        schedule.run_pending() 
+        schedule.run_pending()
         time.sleep(30)
 
-if __name__ == "__main__":
-    task = threading.Thread(target=tweet_job, daemon=True)
+def tweet_job():
+    for post in post_times:
+        schedule.every().day.at(post).do(tweet)
+    for roast in roast_times:
+        schedule.every().day.at(roast).do(reply)
+
+@bot.before_first_request
+def start_scheduler():
+    tweet_job()
+    task = threading.Thread(target=run_scheduler, daemon=True)
     task.start()
+
+if __name__ == "__main__":
     bot.run(host="0.0.0.0", port=8000)
