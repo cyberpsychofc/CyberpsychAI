89caffab9fec968acfdc1f477af41fc78fd9a4f6
cyberpsychofc <aryan2003k@gmail.com>
2025-02-25 09:07:44 +0530

    prod schedule check (test)

--- CyberpsychBot/server.py
+++ CyberpsychBot/server.py
@@ -5,18 +5,20 @@ from flask import Flask
 from tweet import tweet, reply
 
 bot = Flask(__name__)
-
+flag = True
 @bot.route("/")
 def home():
     return "CyberpsychAI is running!"
 
 def tweet_job():
-    tweet()
-    '''schedule.every(1).minutes.do(tweet)
+    global flag
+    if flag:
+        schedule.every(2).minutes.do(tweet)
+        flag = False
     schedule.every(480).minutes.do(reply)
     while True:
         schedule.run_pending() 
-        time.sleep(30)'''
+        time.sleep(30)
 
 if __name__ == "__main__":
     task = threading.Thread(target=tweet_job, daemon=True)
